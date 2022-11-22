eea72084f0d8dcaf6d9f182a3e1774f127e5904b
Son Pham <thesonpham120499@gmail.com>
2022-11-22 10:25:54 +0700

    22-11

--- case_study_angular/furama/src/app/app.module.ts
+++ case_study_angular/furama/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FuramaComponent } from './furama/furama.component';
+import { ListFacilityComponent } from './list-facility/list-facility.component';
+import { EditFacilityComponent } from './edit-facility/edit-facility.component';
+import { AddFacilityComponent } from './add-facility/add-facility.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FuramaComponent
+    FuramaComponent,
+    ListFacilityComponent,
+    EditFacilityComponent,
+    AddFacilityComponent
   ],
   imports: [
     BrowserModule
