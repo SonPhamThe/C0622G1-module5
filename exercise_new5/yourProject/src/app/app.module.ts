0e83c16b7d30fe043b6f7fc1c0ffe8ba3a090253
Son Pham <thesonpham120499@gmail.com>
2022-11-24 20:47:49 +0700

    24-11

--- exercise_new5/yourProject/src/app/app.module.ts
+++ exercise_new5/yourProject/src/app/app.module.ts
@@ -1,17 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { CalculatorComponent } from './calculator/calculator.component';
-import {FormsModule} from '@angular/forms';
-import { ColorComponent } from './color/color.component';
-import { ArticleComponent } from './article/article.component';
-import { LikeComponent } from './like/like.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { NameCardComponent } from './name-card/name-card.component';
-import { ProgressBarComponent } from './progress-bar/progress-bar.component';
-import { RatingBarComponent } from './rating-bar/rating-bar.component';
-import { CountdownTimerComponent } from './countdown-timer/countdown-timer.component';
+import {AppComponent} from './app.component';
+import {CalculatorComponent} from './calculator/calculator.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {ColorComponent} from './color/color.component';
+import {ArticleComponent} from './article/article.component';
+import {LikeComponent} from './like/like.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {NameCardComponent} from './name-card/name-card.component';
+import {ProgressBarComponent} from './progress-bar/progress-bar.component';
+import {RatingBarComponent} from './rating-bar/rating-bar.component';
+import {CountdownTimerComponent} from './countdown-timer/countdown-timer.component';
+import {RegisterFormComponent} from './register-form/register-form.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 @NgModule({
   declarations: [
@@ -24,13 +26,17 @@ import { CountdownTimerComponent } from './countdown-timer/countdown-timer.compo
     NameCardComponent,
     ProgressBarComponent,
     RatingBarComponent,
-    CountdownTimerComponent
+    CountdownTimerComponent,
+    RegisterFormComponent,
+    LoginFormComponent
   ],
-    imports: [
-        BrowserModule,
-        FormsModule
-    ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
